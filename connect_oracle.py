12dbba192ed1bf531ccae13599595271cf6fcfaa
Henrychen128-cmyk <59508976+Henrychen128-cmyk@users.noreply.github.com>
2020-01-06 11:17:33 +0800

    python connect oracle database
    connect database

---
+++ connect_oracle.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+import os
+import cx_Oracle
+import sys
+import importlib
+importlib.reload(sys)
+
+os.environ['NLS_LANG'] = 'TRADITIONAL CHINESE_TAIWAN.AL32UTF8'
+
+TNAME = "'GG'"
+VALUES = "'GG'"
+conn = cx_Oracle.connect("apuser", "apuser", "192.168.255.105:1521/orcl")
+cur = conn.cursor()
+sql = 'INSERT INTO NR_DAILY_LOG (TNAME,VALUE) VALUES ('+TNAME+','+VALUES+')'
+print(sql)
+cur = conn.cursor()
+cur.execute(sql)
+conn.commit()
+'''rows = cur.fetchall()
+for row in rows:
+  print(row)'''
