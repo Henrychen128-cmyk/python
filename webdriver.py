33aeff2fa332ffe19f7aef53abf24bca78842864
Henrychen128-cmyk <59508976+Henrychen128-cmyk@users.noreply.github.com>
2020-01-06 11:27:08 +0800

    automatically search
    automatically search

---
+++ webdriver.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# 使用 Chrome 的 WebDriver
+browser = webdriver.Chrome()
+
+# 開啟 Google 首頁
+browser.get('https://www.google.com.tw')
+
+inputElement = browser.find_element_by_name("q")
+
+inputElement.send_keys("test")
+
+inputElement.submit()
+element = browser.find_element_by_name("LC201b")
+
+for i in element:
+  print(i)
